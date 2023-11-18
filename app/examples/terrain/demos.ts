8b8f9d85c0d6bf9dced4f1bad7fe5bdf1d90d426
Koubae <58447627+Koubae@users.noreply.github.com>
2023-11-18 13:03:06 +0100

    added vite

--- app/examples/terrain/demos.ts
+++
@@ -1,5 +0,0 @@
-import { default as demoTerrain01 } from "./01/lowPolyTerrain.js";
-
-export {
-    demoTerrain01,
-}
--- app/main.ts
+++
@@ -1,7 +0,0 @@
-import App from "./core/app.js";
-import * as demos from "./examples/demos/demos.js";
-
-export {
-    App,
-    demos,
-};
